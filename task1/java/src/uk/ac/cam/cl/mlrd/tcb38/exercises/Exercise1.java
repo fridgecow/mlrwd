186e1a25f8f2419702eaed4c3d8e03f432ef84bd
Tom Benn <tom@fridgecow.com>
2018-01-19 16:34:02 +0000

    Implemented the improved classifier, with a weighting of 2x for strong words. The accuracy has improved from 63.9% to 65.1%

--- task1/java/src/uk/ac/cam/cl/mlrd/tcb38/exercises/Exercise1.java
+++ task1/java/src/uk/ac/cam/cl/mlrd/tcb38/exercises/Exercise1.java
@@ -15,6 +15,9 @@ import java.util.Set;
 import java.util.function.Consumer;
 
 public class Exercise1 implements IExercise1 {
+    public enum Strength {
+        STRONG, WEAK;
+    }
     @Override
     public Map<Path, Sentiment> simpleClassifier(Set<Path> testSet, Path lexiconFile) throws IOException {
         Map<Path, Sentiment> results = new HashMap<>();
@@ -76,6 +79,51 @@ public class Exercise1 implements IExercise1 {
 
     @Override
     public Map<Path, Sentiment> improvedClassifier(Set<Path> testSet, Path lexiconFile) throws IOException {
-        return null;
+        Map<Path, Sentiment> results = new HashMap<>();
+
+        //Load in lexicon file as a "word"->sentiment map
+        Map<String, Sentiment> sentiments = new HashMap<>();
+        Map<String, Strength> strengths = new HashMap<>();
+        try (BufferedReader reader = Files.newBufferedReader(lexiconFile)) {
+            reader.lines().forEach(line -> {
+                String[] tokens = line.split(" ");
+
+                String word = tokens[0].split("=")[1].trim();
+                Sentiment sentiment = tokens[2].split("=")[1].contains("positive") ? Sentiment.POSITIVE : Sentiment.NEGATIVE;
+                Strength strength = tokens[1].split("=")[1].contains("strong") ? Strength.STRONG : Strength.WEAK;
+                sentiments.put(word, sentiment);
+                strengths.put(word, strength);
+            });
+        }
+
+        for(Path test : testSet){
+            //Tokenize each test item
+            List<String> tokens = Tokenizer.tokenize(test);
+
+            //Classify by incrementing for positive and decrementing for negative
+            //Use a 2x weighting on strong words
+            int sentimentCount = 0;
+            for(String token : tokens){
+                int multiplier = 1;
+                if(strengths.containsKey(token) && strengths.get(token).equals(Strength.STRONG)){
+                    multiplier = 2;
+                }
+                if(sentiments.containsKey(token)) {
+                    if (sentiments.get(token).equals(Sentiment.POSITIVE)) {
+                        sentimentCount += multiplier;
+                    } else {
+                        sentimentCount -= multiplier;
+                    }
+                }
+            }
+
+            if(sentimentCount > 0){
+                results.put(test, Sentiment.POSITIVE);
+            }else{
+                results.put(test, Sentiment.NEGATIVE);
+            }
+        }
+
+        return results;
     }
 }
