9afe248c6898e3d5b732203cd8d962c32cbc304c
Tom Benn <tom@fridgecow.com>
2018-01-22 13:44:10 +0000

    Implemented calculation of P(POS) and P(NEG) from the training set

--- task1/java/src/uk/ac/cam/cl/mlrd/tcb38/exercises/Exercise2.java
+++ task1/java/src/uk/ac/cam/cl/mlrd/tcb38/exercises/Exercise2.java
@@ -2,16 +2,33 @@ package uk.ac.cam.cl.mlrd.tcb38.exercises;
 
 import uk.ac.cam.cl.mlrd.exercises.sentiment_detection.IExercise2;
 import uk.ac.cam.cl.mlrd.exercises.sentiment_detection.Sentiment;
+import uk.ac.cam.cl.mlrd.exercises.sentiment_detection.Tokenizer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class Exercise2 implements IExercise2{
     @Override
     public Map<Sentiment, Double> calculateClassProbabilities(Map<Path, Sentiment> trainingSet) throws IOException {
-        return null;
+        Map<Sentiment, Double> probabilities = new HashMap<>();
+        double positive = 0;
+
+        //For each piece of training data
+        for(Sentiment s : trainingSet.values()){
+            if(s.equals(Sentiment.POSITIVE)){
+                positive += 1;
+            }
+        }
+
+        probabilities.put(Sentiment.POSITIVE, positive/trainingSet.size());
+        probabilities.put(Sentiment.NEGATIVE, 1 - positive/trainingSet.size());
+        return probabilities;
     }
 
     @Override
