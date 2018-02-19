3db8b22bad8f5a7b390007666e5031a3b537510f
Tom Benn <tom@fridgecow.com>
2018-02-19 17:12:39 +0000

    Implemented viterbi

--- task1/java/src/uk/ac/cam/cl/mlrd/tcb38/exercises/Exercise8.java
+++ task1/java/src/uk/ac/cam/cl/mlrd/tcb38/exercises/Exercise8.java
@@ -7,13 +7,64 @@ import uk.ac.cam.cl.mlrd.exercises.markov_models.IExercise8;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Exercise8 implements IExercise8 {
     @Override
     public List<DiceType> viterbi(HiddenMarkovModel<DiceRoll, DiceType> model, List<DiceRoll> observedSequence) {
-        return null;
+        List<Map<DiceType, DiceType>> mostProbablePreviousState = new ArrayList<>();
+        List<Map<DiceType, Double>> logPathProbabilities = new ArrayList<>();
+
+        for(int t = 0; t < observedSequence.size(); t++){
+            DiceRoll roll = observedSequence.get(t);
+
+            //System.out.println(logPathProbabilities.get(t-1));
+
+            logPathProbabilities.add(new HashMap<>());
+            mostProbablePreviousState.add(new HashMap<>());
+            for(DiceType state : DiceType.values()){
+                //Compute max(delta(t-1)*...), but log'd
+                Double max = Double.NEGATIVE_INFINITY;
+                double rollProb = Math.log(model.getEmissionMatrix().get(state).get(roll));
+                DiceType bestState = null;
+
+                //System.out.println("Roll Probability of "+observedSequence.get(t)+": "+rollProb);
+
+                if(t == 0){
+                    max = rollProb;
+                }else {
+                    for (DiceType i : DiceType.values()) {
+                        Double candidate = logPathProbabilities.get(t - 1).get(i);
+                        candidate += Math.log(model.getTransitionMatrix().get(i).get(state));
+                        candidate += rollProb;
+
+                        //System.out.println(i.toString()+"->"+state.toString()+": "+Math.log(model.getTransitionMatrix().get(i).get(state)));
+                        if (bestState == null || candidate > max) {
+                            max = candidate;
+                            bestState = i;
+                        }
+                    }
+
+                    mostProbablePreviousState.get(t).put(state, bestState);
+                }
+
+                //System.out.println("Best: "+bestState+" @ "+max);
+                logPathProbabilities.get(t).put(state, max);
+            }
+        }
+
+        //Backtrack
+        List<DiceType> bestSequence = new ArrayList<>();
+
+        //Find max probability final dice
+        DiceType state = DiceType.END;
+        bestSequence.add(state);
+        for(int i = mostProbablePreviousState.size() - 1; i > 0; i--){
+            state = mostProbablePreviousState.get(i).get(state);
+            bestSequence.add(state);
+        }
+        Collections.reverse(bestSequence);
+        return bestSequence;
     }
 
     @Override
