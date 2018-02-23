96f779bf0d974bcd37734c70321350d6b1cde800
Tom Benn <tom@fridgecow.com>
2018-02-23 15:18:30 +0000

    Implemented Exercise 9 by copying code from Exercise 8 and 7.

--- task1/java/src/uk/ac/cam/cl/mlrd/tcb38/exercises/Exercise9.java
+++ task1/java/src/uk/ac/cam/cl/mlrd/tcb38/exercises/Exercise9.java
@@ -3,38 +3,186 @@ package uk.ac.cam.cl.mlrd.tcb38.exercises;
 import uk.ac.cam.cl.mlrd.exercises.markov_models.*;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Exercise9 implements IExercise9 {
 
     @Override
     public HiddenMarkovModel<AminoAcid, Feature> estimateHMM(List<HMMDataStore<AminoAcid, Feature>> sequencePairs) throws IOException {
-        return null;
+        Map<Feature, Map<Feature, Double>> transitionMatrix = new HashMap<>();
+        Map<Feature, Map<AminoAcid, Double>> emissionMatrix = new HashMap<>();
+
+        //Calculate total transitions from each state, and the transition counts from one to the next
+        Map<Feature, Double> transitionTotals = new HashMap<>();
+        Map<Feature, Double> emissionTotals = new HashMap<>();
+        for (HMMDataStore<AminoAcid, Feature> d : sequencePairs) {
+            for (int i = 0; i < d.hiddenSequence.size(); i++) {
+                Feature current = d.hiddenSequence.get(i);
+                AminoAcid currentRoll = d.observedSequence.get(i);
+
+                //Emission counts
+                emissionTotals.put(current, emissionTotals.getOrDefault(current, 0.0)+1);
+                Map<AminoAcid, Double> currentEmissions = emissionMatrix.getOrDefault(current, new HashMap<>());
+                currentEmissions.put(currentRoll, currentEmissions.getOrDefault(currentRoll, 0.0)+1);
+                emissionMatrix.put(current, currentEmissions);
+
+                //Transition counts
+                if(i < d.hiddenSequence.size() - 1) {
+                    Feature next = d.hiddenSequence.get(i + 1);
+                    transitionTotals.put(current, transitionTotals.getOrDefault(current, 0.0) + 1);
+
+                    Map<Feature, Double> currentMap = transitionMatrix.getOrDefault(current, new HashMap<>());
+                    currentMap.put(next, currentMap.getOrDefault(next, 0.0) + 1);
+                    transitionMatrix.put(current, currentMap);
+                }
+            }
+        }
+
+        //Divide the transition and emission counts by the total transitions and emissions
+        for (Feature type : Feature.values()) {
+            for(Feature nextType : Feature.values()){
+                if(transitionTotals.containsKey(type)) {
+                    transitionMatrix.get(type).put(
+                            nextType,
+                            transitionMatrix.get(type).getOrDefault(nextType, 0.0) / transitionTotals.get(type)
+                    );
+                }else{
+                    Map<Feature, Double> transitionMap = transitionMatrix.getOrDefault(type, new HashMap<>());
+                    transitionMap.put(nextType, 0.0);
+                    transitionMatrix.put(type, transitionMap);
+                }
+            }
+
+            for(AminoAcid roll : AminoAcid.values()){
+                emissionMatrix.get(type).put(
+                        roll,
+                        emissionMatrix.get(type).getOrDefault(roll, 0.0)/emissionTotals.get(type)
+                );
+            }
+        }
+
+        return new HiddenMarkovModel<>(transitionMatrix, emissionMatrix);
     }
 
     @Override
     public List<Feature> viterbi(HiddenMarkovModel<AminoAcid, Feature> model, List<AminoAcid> observedSequence) {
-        return null;
+        List<Map<Feature, Feature>> mostProbablePreviousState = new ArrayList<>();
+        List<Map<Feature, Double>> logPathProbabilities = new ArrayList<>();
+
+        for(int t = 0; t < observedSequence.size(); t++){
+            AminoAcid roll = observedSequence.get(t);
+
+            //System.out.println(logPathProbabilities.get(t-1));
+
+            logPathProbabilities.add(new HashMap<>());
+            mostProbablePreviousState.add(new HashMap<>());
+            for(Feature state : Feature.values()){
+                //Compute max(delta(t-1)*...), but log'd
+                Double max = Double.NEGATIVE_INFINITY;
+                double rollProb = Math.log(model.getEmissionMatrix().get(state).get(roll));
+                Feature bestState = null;
+
+                //System.out.println("Roll Probability of "+observedSequence.get(t)+": "+rollProb);
+
+                if(t == 0){
+                    max = rollProb;
+                }else {
+                    for (Feature i : Feature.values()) {
+                        Double candidate = logPathProbabilities.get(t - 1).get(i);
+                        candidate += Math.log(model.getTransitionMatrix().get(i).get(state));
+                        candidate += rollProb;
+
+                        //System.out.println(i.toString()+"->"+state.toString()+": "+Math.log(model.getTransitionMatrix().get(i).get(state)));
+                        if (bestState == null || candidate > max) {
+                            max = candidate;
+                            bestState = i;
+                        }
+                    }
+
+                    mostProbablePreviousState.get(t).put(state, bestState);
+                }
+
+                //System.out.println("Best: "+bestState+" @ "+max);
+                logPathProbabilities.get(t).put(state, max);
+            }
+        }
+
+        //Backtrack
+        List<Feature> bestSequence = new ArrayList<>();
+
+        //Find max probability final dice
+        Feature state = Feature.END;
+        bestSequence.add(state);
+        for(int i = mostProbablePreviousState.size() - 1; i > 0; i--){
+            state = mostProbablePreviousState.get(i).get(state);
+            bestSequence.add(state);
+        }
+        Collections.reverse(bestSequence);
+        return bestSequence;
     }
 
     @Override
     public Map<List<Feature>, List<Feature>> predictAll(HiddenMarkovModel<AminoAcid, Feature> model, List<HMMDataStore<AminoAcid, Feature>> testSequencePairs) throws IOException {
-        return null;
+        Map<List<Feature>, List<Feature>> predictions = new HashMap<>();
+
+        for(HMMDataStore<AminoAcid, Feature> data : testSequencePairs){
+            predictions.put(data.hiddenSequence, viterbi(model, data.observedSequence));
+        }
+
+        return predictions;
     }
 
     @Override
     public double precision(Map<List<Feature>, List<Feature>> true2PredictedMap) {
-        return 0;
+        double correctCount = 0;
+        double allCount = 0;
+
+        for(List<Feature> trueHidden : true2PredictedMap.keySet()){
+            List<Feature> predictedHidden = true2PredictedMap.get(trueHidden);
+
+            for(int i = 0; i < trueHidden.size(); i++){
+                if(predictedHidden.get(i).equals(Feature.MEMBRANE)) {
+                    allCount++;
+
+                    if (trueHidden.get(i).equals(Feature.MEMBRANE)) {
+                        correctCount++;
+                    }
+                }
+            }
+        }
+
+        return correctCount / allCount;
     }
 
     @Override
     public double recall(Map<List<Feature>, List<Feature>> true2PredictedMap) {
-        return 0;
+        double correctCount = 0;
+        double allCount = 0;
+
+        for(List<Feature> trueHidden : true2PredictedMap.keySet()){
+            List<Feature> predictedHidden = true2PredictedMap.get(trueHidden);
+
+            for(int i = 0; i < trueHidden.size(); i++){
+                if(predictedHidden.get(i).equals(Feature.MEMBRANE)) {
+                    if (trueHidden.get(i).equals(Feature.MEMBRANE)) {
+                        correctCount++;
+                    }
+                }
+
+                if(trueHidden.get(i).equals(Feature.MEMBRANE)){
+                    allCount++;
+                }
+            }
+        }
+
+        return correctCount / allCount;
     }
 
     @Override
     public double fOneMeasure(Map<List<Feature>, List<Feature>> true2PredictedMap) {
-        return 0;
+        double precision = precision(true2PredictedMap);
+        double recall = recall(true2PredictedMap);
+
+        return (2*precision*recall)/(precision + recall);
     }
 }
--- task1/java/src/uk/ac/cam/cl/mlrd/testing/Exercise9Tester.java
+++ task1/java/src/uk/ac/cam/cl/mlrd/testing/Exercise9Tester.java
@@ -5,15 +5,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import uk.ac.cam.cl.mlrd.exercises.sentiment_detection.IExercise5;
+import uk.ac.cam.cl.mlrd.tcb38.exercises.Exercise5;
 import uk.ac.cam.cl.mlrd.tcb38.exercises.Exercise9;
 import uk.ac.cam.cl.mlrd.exercises.markov_models.AminoAcid;
 import uk.ac.cam.cl.mlrd.exercises.markov_models.Feature;
@@ -25,53 +23,77 @@ public class Exercise9Tester {
 
 	static final Path dataFile = Paths.get("data/bio_dataset.txt");
 
+	public static List<List<HMMDataStore<AminoAcid, Feature>>> stratifiedRandomSplit(List<HMMDataStore<AminoAcid, Feature>> dataSet, int seed){
+		List<List<HMMDataStore<AminoAcid, Feature>>> folds = new ArrayList<>();
+		Random rand = new Random(seed);
+
+		Collections.shuffle(dataSet, rand);
+
+		int i = 0;
+		for(HMMDataStore<AminoAcid, Feature> p: dataSet){
+			if(i < 10){
+				//Initialise List
+				List<HMMDataStore<AminoAcid, Feature>> l = new ArrayList<>();
+				l.add(p);
+				folds.add(l);
+			}else{
+				//Add to list
+				folds.get(i % 10).add(p);
+			}
+			i++;
+		}
+
+		return folds;
+	}
+
 	public static void main(String[] args) throws IOException {
 
 		List<HMMDataStore<AminoAcid, Feature>> sequencePairs = HMMDataStore.loadBioFile(dataFile);
 
-		// Use for testing the code
-		Collections.shuffle(sequencePairs, new Random(0));
-		int testSize = sequencePairs.size() / 10;
-		List<HMMDataStore<AminoAcid, Feature>> devSet = sequencePairs.subList(0, testSize);
-		List<HMMDataStore<AminoAcid, Feature>> testSet = sequencePairs.subList(testSize, 2 * testSize);
-		List<HMMDataStore<AminoAcid, Feature>> trainingSet = sequencePairs.subList(testSize * 2, sequencePairs.size());
-		// But:
-		// TODO: Replace with cross-validation for the tick.
+		List<List<HMMDataStore<AminoAcid, Feature>>> folds = stratifiedRandomSplit(sequencePairs, 0);
 
-		IExercise9 implementation = (IExercise9) new Exercise9();
+		IExercise9 implementation = new Exercise9();
+		IExercise5 implementation5 = (IExercise5) new Exercise5();
 
-		HiddenMarkovModel<AminoAcid, Feature> model = implementation.estimateHMM(trainingSet);
-		System.out.println("Predicted transitions:");
-		System.out.println(model.getTransitionMatrix());
-		System.out.println();
-		System.out.println("Predicted emissions:");
-		System.out.println(model.getEmissionMatrix());
+		double[] precisions = new double[10];
+		double[] recalls = new double[10];
+		double[] fOneMeasure = new double[10];
+		for(int i = 0; i < 10; i++){
+			List<HMMDataStore<AminoAcid, Feature>> testSet = new ArrayList<>();
+			List<HMMDataStore<AminoAcid, Feature>> trainingSet = new ArrayList<>();
+			for(int j = 0; j < 10; j++){
+				if(i != j){
+					testSet.addAll(folds.get(j));
+				}else{
+					trainingSet.addAll(folds.get(j));
+				}
+			}
+
+			HiddenMarkovModel<AminoAcid, Feature> model = implementation.estimateHMM(trainingSet);
+			Map<List<Feature>, List<Feature>> true2PredictedMap = implementation.predictAll(model, testSet);
+
+			precisions[i] = implementation.precision(true2PredictedMap);
+			recalls[i] = implementation.recall(true2PredictedMap);
+			fOneMeasure[i] = implementation.fOneMeasure(true2PredictedMap);
+		}
+
+		System.out.println("10-fold cross-validation\n");
+
+		System.out.println("Precisions:");
+		System.out.println(Arrays.toString(precisions));
+		System.out.println("Mean: "+implementation5.cvAccuracy(precisions));
+		System.out.println("Variance: "+implementation5.cvVariance(precisions));
+		System.out.println("");
+
+		System.out.println("Recalls:");
+		System.out.println(Arrays.toString(recalls));
+		System.out.println("Mean: "+implementation5.cvAccuracy(recalls));
+		System.out.println("Variance: "+implementation5.cvVariance(recalls));
 		System.out.println();
 
-		HMMDataStore<AminoAcid, Feature> data = devSet.get(0);
-		List<Feature> predicted = implementation.viterbi(model, data.observedSequence);
-		System.out.println("True hidden sequence:");
-		System.out.println(data.hiddenSequence);
-		System.out.println();
-
-		System.out.println("Predicted hidden sequence:");
-		System.out.println(predicted);
-		System.out.println();
-
-		Map<List<Feature>, List<Feature>> true2PredictedSequences = implementation.predictAll(model, devSet);
-		double accuracy = implementation.precision(true2PredictedSequences);
-		System.out.println("Prediction precision:");
-		System.out.println(accuracy);
-		System.out.println();
-
-		double recall = implementation.recall(true2PredictedSequences);
-		System.out.println("Prediction recall:");
-		System.out.println(recall);
-		System.out.println();
-
-		double f1Score = implementation.fOneMeasure(true2PredictedSequences);
-		System.out.println("Prediction F1 score:");
-		System.out.println(f1Score);
-		System.out.println();
+		System.out.println("fOneMeasure:");
+		System.out.println(Arrays.toString(fOneMeasure));
+		System.out.println("Mean: "+implementation5.cvAccuracy(fOneMeasure));
+		System.out.println("Variance: "+implementation5.cvVariance(fOneMeasure));
 	}
 }
