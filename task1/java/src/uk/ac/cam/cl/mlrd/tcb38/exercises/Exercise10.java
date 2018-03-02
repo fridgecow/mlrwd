3018f2b011e1a100e9136d07df6f7d094b6e580a
Tom Benn <tom@fridgecow.com>
2018-03-02 15:09:34 +0000

    Implemented Exercise 10

--- task1/java/src/uk/ac/cam/cl/mlrd/tcb38/exercises/Exercise10.java
+++ task1/java/src/uk/ac/cam/cl/mlrd/tcb38/exercises/Exercise10.java
@@ -2,24 +2,83 @@ package uk.ac.cam.cl.mlrd.tcb38.exercises;
 
 import uk.ac.cam.cl.mlrd.exercises.social_networks.IExercise10;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Exercise10 implements IExercise10{
     @Override
     public Map<Integer, Set<Integer>> loadGraph(Path graphFile) throws IOException {
-        return null;
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+
+        BufferedReader reader = Files.newBufferedReader(graphFile);
+        reader.lines().forEach(line -> {
+            String[] nodes = line.split(" ");
+            Integer u = Integer.valueOf(nodes[0]);
+            Integer v = Integer.valueOf(nodes[1]);
+
+            if(!graph.containsKey(u)){
+                graph.put(u, new HashSet<>());
+            }
+
+            graph.get(u).add(v);
+
+            //Undirected graph
+            if(!graph.containsKey(v)){
+                graph.put(v, new HashSet<>());
+            }
+
+            graph.get(v).add(u);
+        });
+
+        return graph;
     }
 
     @Override
     public Map<Integer, Integer> getConnectivities(Map<Integer, Set<Integer>> graph) {
-        return null;
+        Map<Integer, Integer> connectivities = new HashMap<>();
+        for(Integer node : graph.keySet()){
+            connectivities.put(node, graph.get(node).size());
+        }
+
+        return connectivities;
     }
 
     @Override
     public int getDiameter(Map<Integer, Set<Integer>> graph) {
-        return 0;
+        int shortestLongPath = Integer.MAX_VALUE;
+
+        for(Integer startNode : graph.keySet()){
+            //BFS from this node
+            Queue<Integer> queue = new LinkedList<>();
+            Map<Integer,Integer> visitedDistance = new HashMap<>();
+            int longestPath = 0;
+
+            visitedDistance.put(startNode, 0);
+            queue.add(startNode);
+
+            Integer node;
+            while ((node = queue.poll()) != null ){
+                int nodeDistance = visitedDistance.get(node);
+                for(Integer neighbour : graph.get(node)){
+                    if(!visitedDistance.containsKey(neighbour)){
+                        visitedDistance.put(neighbour, nodeDistance + 1);
+                        queue.add(neighbour);
+                    }
+                }
+
+                if(nodeDistance > longestPath){
+                    longestPath = nodeDistance;
+                }
+            }
+
+            if(longestPath < shortestLongPath){
+                shortestLongPath = longestPath;
+            }
+        }
+
+        return shortestLongPath*2;
     }
 }
